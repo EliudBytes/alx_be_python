cd1f36565f391e0748cedd655849c2db814bae40
EliudBytes <mathueliud1@gmail.com>
2025-11-21 23:23:47 +0300

    Refactor BankAccount to use public balance attribute

--- bank_account.py
+++ bank_account.py
@@ -1,17 +1,18 @@
-# bank_account.py
-
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance
+    def __init__(self, balance=0):
+        self.balance = balance
 
     def deposit(self, amount):
-        self.__account_balance += amount
+        if amount > 0:
+            self.balance += amount
+            return True
+        return False
 
     def withdraw(self, amount):
-        if amount <= self.__account_balance:
-            self.__account_balance -= amount
+        if 0 < amount <= self.balance:
+            self.balance -= amount
             return True
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current Balance: ${self.balance}")
